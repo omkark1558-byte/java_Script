716452f3e39625673382e52d42ee3da0c5ec3bd1
omkark1558-byte <omkark1558@gmail.com>
2026-05-30 12:06:09 +0530

    js

---
+++ Es6_features/Scope_1.js
@@ -0,0 +1,19 @@
+ 
+var name = "Bye";
+if(true)
+{
+    var name = "Hey";
+}
+console.log(name);
+// console.log(name);
+
+
+
+// let name = "Jay Shree Ram 🙏🏻"
+
+// while(true)
+// {
+//     console.log(name);
+//     return false;
+// }
+// console.log(name);
