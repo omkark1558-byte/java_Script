95b4a10718c1255334c9a31aafda56042d74cf99
omkark1558-byte <omkark1558@gmail.com>
2026-05-31 11:02:32 +0530

    Closer's Revsion

--- Es6_features/Closer_1.js
+++ Es6_features/Closer_1.js
@@ -1,30 +1,24 @@
 
-function Banking()
-  {
-    let bank_balance=15000;
+function Banking() {
+    let bank_balance = 15000;
 
-    function Deposit(c)
-    {
-        bank_balance = bank_balance+=c;
-        console.log("Deposited Amount ✅ \n"+"Available balance is :"+ bank_balance);
+    function Deposit(c) {
+        bank_balance = bank_balance += c;
+        console.log("Deposited Amount ✅ \n" + "Available balance is :" + bank_balance);
     }
-    function Withdrawal(v)
-    {
-        if(v>bank_balance)
-        {
+    function Withdrawal(v) {
+        if (v > bank_balance) {
             console.log("Insufficient balance...");
         }
-        else
-        {
-            bank_balance = bank_balance-=v;
-            console.log("Withdrwal Amount..⚠️: "+v+"\n"+"Available balance is :"+ bank_balance);
+        else {
+            bank_balance = bank_balance -= v;
+            console.log("Withdrwal Amount..⚠️: " + v + "\n" + "Available balance is :" + bank_balance);
         }
     }
     return [Deposit, Withdrawal];
-  }
-  let [Deposit,Withdrawal]= Banking();
-  Deposit(5000);
-  Withdrawal(4859);
+}
+let [Deposit, Withdrawal] = Banking();
+Deposit(5000);
+Withdrawal(4859);
 
-   
-  
+
